7af4486056f9fec686a4d7e55d31b38645224319
ggomjae <ggomjae@naver.com>
2020-04-04 12:33:20 +0900

    Initial commit

---
+++ src/main/java/com/ggomjae/web/toywebservice/ToywebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ggomjae.web.toywebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToywebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToywebserviceApplication.class, args);
+    }
+
+}
